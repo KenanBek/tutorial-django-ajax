a13598dc3b6d2fee4d37a627761b29ed5ac58206
KenanBek <mail@kenanbek.me>
2014-08-05 22:59:14 +0500

    Sample 2 files.

--- static/sample2/index.js
+++ static/sample2/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by kenanbek on 8/5/14.
  */
+
+$(document).ready(function () {
+    $('#rootwizard').bootstrapWizard({
+        onTabShow: function (tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index + 1;
+            var $percent = ($current / $total) * 100;
+            $('#form-progress').css({width: $percent + '%'});
+        }
+    });
+});
