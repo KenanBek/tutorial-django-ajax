0a85b1207ab1fd4f4751d03da2c2c0744a98f5b1
KenanBek <mail@kenanbek.me>
2014-08-06 17:10:52 +0500

    Removed copyright text from file.

--- static/sample2/index.js
+++ static/sample2/index.js
@@ -1,14 +1,9 @@
-/**
- * Created by kenanbek on 8/5/14.
- */
-
 $(document).ready(function () {
     $('#rootwizard').bootstrapWizard({
         onTabShow: function (tab, navigation, index) {
             var $total = navigation.find('li').length;
             var $current = index + 1;
             var $percent = ($current / $total) * 100;
-            console.log(parseInt($percent));
             $('#rootwizard').find('.bar').css({width:$percent+'%'});
         },
         onNext: function (tab, navigation, index) {
